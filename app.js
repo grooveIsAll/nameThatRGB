56754b521486db47d850b089c6d27bb4a789b41f
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-13 13:20:51 -0700

    [feat]: add logic to choose and randomize the picked color

--- app.js
+++ app.js
@@ -8,7 +8,37 @@ const colors = [
 ];
 
 let squares = document.querySelectorAll(".square");
+let pickedColor = pickColor();
+let colorDisplay = document.getElementById("rgb");
+let alert = document.getElementById('alert');
+
+colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++) {
+  // add colors to squares
   squares[i].style.background = colors[i];
+  // add 'click' listeners to squares
+  squares[i].addEventListener("click", function() {
+    let clickedColor = this.style.background;
+    if(clickedColor === pickedColor) {
+      alert.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else {
+      this.style.background = "#232323";
+      alert.textContent = "Try Again";
+    }
+  });
+}
+
+function changeColors(color) {
+  for(let i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+  let titleBackground = document.querySelector("h1");
+  titleBackground.style.background = color;
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
